4d6e2171fb6c3eaeeedd3ae601ee8fc2a4b2c44c
rahulsoni <v-rahusoni@expediagroup.com>
2025-07-11 13:40:34 +0530

    Stream API - flatMap

--- src/main/java/com/dsa/learning/java8/streams/intermediate/flatMap/FlatMapUsages.java
+++ src/main/java/com/dsa/learning/java8/streams/intermediate/flatMap/FlatMapUsages.java
@@ -27,6 +27,17 @@ public class FlatMapUsages {
         // 05. Combination with map, filter, and distinct ....
         combineFlatMapWithOtherOperations();
 
+        // 06. Flatten a List of Arrays ....
+        flattenAListOfArrays();
+
+        // 07. Flatten a List of Files and Extract File Names ....
+        flattenAListOfFilesAndExtractFileNames();
+
+        // 08. Flatten a Multi-Level List ....
+        flattenAMultiLevelList();
+
+        // 09. Combine Two Streams Using flatMap() ....
+        combineTwoStreams();
     }
 
     /**
@@ -96,7 +107,7 @@ public class FlatMapUsages {
         );
 
         List<String> sentenceOutput = sentences.stream()
-                .flatMap(sentence -> List.of(sentence.split(" ")).stream())
+                .flatMap(sentence -> List.of(sentence.split(" ")).stream()) // Since, we require a list here to convert it into a Stream. So we're first creating a List of Words and then creating a stream() out of it ....
                 .distinct()
                 .collect(Collectors.toList());
         System.out.println("Sentence: " + sentenceOutput);
@@ -205,6 +216,139 @@ public class FlatMapUsages {
         addDivider();
     }
 
+    /**
+     * 06. flattenAListOfArrays
+     * You have a List<String[]> where each array contains strings. Convert it into a flattened list of strings.
+     *
+     * Explanation:
+     * Arrays::stream:
+     * Converts each array (String[]) into a stream.
+     *
+     * flatMap(Arrays::stream) OR flatMap(arr -> List.of(arr).stream()):
+     * Works on each array, flattens them into a single stream of String. By first converting them into a list and then into a stream.
+     *
+     * collect(Collectors.toList()):
+     * Collects all strings into one flat list.
+     */
+    public static void flattenAListOfArrays(){
+        System.out.println("**** flattenAListOfArrays() ****");
+
+        List<String[]> stringArrays = List.of(
+                new String[]{"a", "b", "c"},
+                new String[]{"d", "e"},
+                new String[]{"f", "g", "h"}
+        );
+
+        List<String> flattenedListOfStrings = stringArrays.stream()
+                        .flatMap(arr -> List.of(arr).stream()) // flatMap(Arrays::stream): would also work the same. Concept is to create a List of Array elements & then convert it into a stream ....
+                                .collect(Collectors.toUnmodifiableList());
+
+        System.out.println("Flattened List Of Strings: " + flattenedListOfStrings);
+
+        addDivider();
+    }
+
+
+    /**
+     * 07. Flatten a List of Files and Extract File Names.
+     * You have a List<Directory> where each directory contains a list of files. Flatten all filenames into a single list.
+     *
+     * Explanation:
+     * dir -> dir.files.stream():
+     * For each Directory, get its stream of files.
+     *
+     * flatMap(dir -> dir.files.stream()):
+     * Flattens all streams into one continuous stream.
+     *
+     * Result:
+     * All filenames in a single flat list.
+     */
+    public static void flattenAListOfFilesAndExtractFileNames(){
+        System.out.println("**** flattenAListOfFilesAndExtractFileNames() ****");
+
+        List<Directory> directories = List.of(
+                new Directory("Dir1", List.of("file1.txt", "file2.txt")),
+                new Directory("Dir2", List.of("file3.txt")),
+                new Directory("Dir3", List.of("file4.txt", "file5.txt", "file6.txt"))
+        );
+
+        List<String> directoryNamesList = directories.stream()
+                        .flatMap(dir -> dir.getFiles().stream())
+                                .collect(Collectors.toUnmodifiableList());
+
+        System.out.println("Flattened List Of Directories: " + directoryNamesList);
+
+        addDivider();
+    }
+
+    /**
+     * 08. Flatten a Multi-Level List Using flatMap()
+     * You have a deeply nested list (List<List<List<Integer>>>) and need to flatten it to a single list of integers.
+     *
+     * Explanation:
+     * Level-Wise Flattening:
+     * First .flatMap(level1 -> level1.stream()): Flattens List<List<List<Integer>>> into List<List<Integer>>.
+     * Second .flatMap(level2 -> level2.stream()): Flattens List<List<Integer>> into List<Integer>.
+     *
+     * Final Result:
+     * Single flat list of integers.
+     */
+    public static void flattenAMultiLevelList(){
+        System.out.println("**** flattenAMultiLevelList() ****");
+
+        List<List<List<Integer>>> deepNestedLists = List.of(
+                List.of(List.of(1, 2, 3), List.of(4, 5)),
+                List.of(List.of(6, 7), List.of(8, 9)),
+                List.of(List.of(10, 11))
+        );
+
+        List<Integer> multiLevelFlatList = deepNestedLists.stream()
+                        .flatMap(subList1 -> subList1.stream()) // First flattening, gives us a List that contains internally lists ....
+                                .flatMap(subList2 -> subList2.stream()) // This flatenning gives us the elements within internal lists ....
+                                        .collect(Collectors.toUnmodifiableList());
+
+        System.out.println("Flattened Multi Level List: " + multiLevelFlatList);
+
+        addDivider();
+    }
+
+
+    /**
+     * 09. Combine Two Streams Using flatMap()
+     * You have two lists (List<String> and List<Integer>) and need to create all possible pairs (Cartesian Product).
+     *
+     * Main concept behind this is that we need to stream over each element of list1 and then for each such element we need to create a stream of Second list.
+     * We can then apply Map method to second stream and add both these elements together which are finnaly collected into a final List.
+     *
+     * Explanation:
+     * Outer Stream (letters.stream()):
+     * Iterates over each letter (A, B, C).
+     *
+     * Inner Stream (numbers.stream()):
+     * Iterates over each number (1, 2) for every letter.
+     *
+     * flatMap(...):
+     * Combines letter and number to form pairs.
+     */
+    public static void combineTwoStreams(){
+        System.out.println("**** combineTwoStreams() ****");
+
+        List<String> letters = List.of("A", "B", "C");
+        List<Integer> numbers = List.of(1, 2);
+
+        List<String> cartesianProduct = letters.stream()
+                        .flatMap(letter ->  numbers.stream() // Second Stream of List created inside the stream of First list ....
+                                .map(number -> letter + number))
+                                .collect(Collectors.toUnmodifiableList());
+
+        System.out.println("Cartesian Product by combining Two Streams: " + cartesianProduct);
+
+        addDivider();
+    }
+
+
+
+
     public static void addDivider(){
         System.out.println("\n------------------------------------------");
     }
@@ -232,6 +376,32 @@ public class FlatMapUsages {
 
         public void setSubcategories(List<String> subcategories) {
             this.subcategories = subcategories;
+        }
+    }
+
+    static class Directory {
+        String name;
+        List<String> files;
+
+        Directory(String name, List<String> files) {
+            this.name = name;
+            this.files = files;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<String> getFiles() {
+            return files;
+        }
+
+        public void setFiles(List<String> files) {
+            this.files = files;
         }
     }
 
