eec5ce4085abb0ab7652289564205f32b941308f
rahulsoni <v-rahusoni@expediagroup.com>
2025-07-17 06:54:13 +0530

    Completable Future - Basics

--- src/main/java/com/dsa/learning/java8/completable_future/ChainingCompletableFutures.java
+++ src/main/java/com/dsa/learning/java8/completable_future/ChainingCompletableFutures.java
@@ -13,12 +13,15 @@ public class ChainingCompletableFutures {
         });
 
         // Step 2: Create the second CompletableFuture that depends on the result of future1 ....
+        // thenApply(): It transforms the result of a CompletableFuture. It’s used when you want to perform further computation on the result synchronously.
         CompletableFuture<Integer> completableFuture2 = completableFuture1.thenApply(resultFromFirstCF -> {
             System.out.println("Executing second task...");
             return resultFromFirstCF * 4;
         });
 
         // Step 3: Create the third CompletableFuture for consuming the transformed result ....
+        // thenAccept(): It is used for tasks that handle the result without returning a value.
+        // If you want to consume the result but don’t need to pass anything further down the chain, use thenAccept().
         CompletableFuture<Void> completableFuture3 = completableFuture2.thenAccept(resultFromSecondCF -> {
             System.out.println("Executing third task with final output: " + resultFromSecondCF);
         });
